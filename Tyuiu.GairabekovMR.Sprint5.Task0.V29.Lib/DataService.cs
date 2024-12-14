decdafecbc5e9f4ad575f73352cb77b6d5cbca31
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 06:57:22 +0500

    Add project files.

---
+++ Tyuiu.GairabekovMR.Sprint5.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.GairabekovMR.Sprint5.Task0.V29.Lib
+{
+    public class DataService : ISprint5Task0V29
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+
+            double f = 6.1 * Math.Pow(x, 3) + 0.23 * Math.Pow(x, 2) + 1.04 * x;
+
+            f = Math.Round(f, 2);
+            File.WriteAllText(path, Convert.ToString(f));
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint5.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Tyuiu.GairabekovMR.Sprint5.Task0.V29.Lib;
+
+namespace Tyuiu.GairabekovMR.Sprint5.Task0.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+
+            FileInfo fileinfo = new FileInfo(path);
+
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+
+}
