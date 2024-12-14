627dcd7a2270f8894029c73049781aad2eceed4c
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 08:22:07 +0500

    Task5

---
+++ Tyuiu.GairabekovMR.Sprint5.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.GairabekovMR.Sprint5.Task5.V2.Lib
+{
+    public class DataService : ISprint5Task5V2
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double count = 0;
+            double sum = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (Convert.ToDouble(line) > 0)
+                    {
+                        sum = sum + Convert.ToDouble(line);
+                        count += 1;
+                    }
+                }
+                double res = sum / count;
+                return Math.Round(res, 3);
+            }
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint5.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+using Tyuiu.GairabekovMR.Sprint5.Task5.V2.Lib;
+namespace Tyuiu.GairabekovMR.Sprint5.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\User\source\repos\Tyuiu.Gairab.Sprint5\Tyuiu.Gairab.Sprint5.Task5.V2\bin\Debug\InPutFileTask5.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
