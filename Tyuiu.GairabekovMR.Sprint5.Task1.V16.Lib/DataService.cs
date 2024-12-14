2ed0af213a4339e9601b7a5a3807c4675163a22c
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 07:17:03 +0500

    Task1

--- Tyuiu.GairabekovMR.Sprint5.Task1.V16.Lib/DataService.cs
+++ Tyuiu.GairabekovMR.Sprint5.Task1.V16.Lib/DataService.cs
@@ -1,23 +1,25 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
-namespace Tyuiu.GairabekovMR.Sprint5.Task1.V16.Lib;
-public class DataService : ISprint5Task1V16
+namespace Tyuiu.GairabekovMR.Sprint5.Task1.V16.Lib
 {
-    public string SaveToFileTextData(int startValue, int stopValue)
+    public class DataService : ISprint5Task1V16
     {
-        string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
-
-        while (startValue <= stopValue)
+        public string SaveToFileTextData(int startValue, int stopValue)
         {
-            double result = Math.Sin(startValue) + (2 * startValue) / 3 - Math.Cos(startValue) * 4 * startValue;
-            if (double.IsInfinity(result) || double.IsNaN(result))
-                result = 0;
-            result = Math.Round(result, 2);
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
 
-            File.AppendAllText(path, $"{result.ToString()}\n");
-            startValue++;
+            while (startValue <= stopValue)
+            {
+                double result = Math.Sin(startValue) + (2 * startValue) / 3 - Math.Cos(startValue) * 4 * startValue;
+                if (double.IsInfinity(result) || double.IsNaN(result))
+                    result = 0;
+                result = Math.Round(result, 2);
+
+                File.AppendAllText(path, $"{result.ToString()}\n");
+                startValue++;
+            }
+
+            return path;
         }
-
-        return path;
     }
 }
