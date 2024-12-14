7b87b1d35271d8205ff6915ca7866303a67af3a2
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 07:33:26 +0500

    Task2

---
+++ Tyuiu.GairabekovMR.Sprint5.Task2.V2.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.GairabekovMR.Sprint5.Task2.V2.Lib;
+
+namespace Tyuiu.GairabekovMR.Sprint5.Task2.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+            FileInfo fileInfo = new FileInfo(path);
+            DataService ds = new DataService();
+            int[,] mtrx = new int[3, 3] { {-7,7,5 },
+                                          {4,2,-7 },
+                                          {2,6,-4 }};
+            var res = ds.SaveToFileTextData(mtrx);
+            string wait = "0;1;1\r\n" +
+                "1;1;0\r\n" +
+                "1;1;0";
+            Assert.AreEqual(wait, File.ReadAllText(res));
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint5.Task2.V2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GairabekovMR.Sprint5.Task2.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
