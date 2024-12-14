9e02574eeb3dffedc0cc1e30d116339b13ccd008
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 08:35:52 +0500

    Task6

---
+++ Tyuiu.GairabekovMR.Sprint5.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.GairabekovMR.Sprint5.Task6.V21.Lib;
+namespace Tyuiu.GairabekovMR.Sprint5.Task6.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\���������\source\repos\Tyuiu.Gairab.Sprint5\DataSprint5\InPutDataFileTask6V21.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 2;
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            string path = @"C:\Users\User\source\repos\Tyuiu.Gairab.Sprint5\DataSprint5\InPutDataFileTask6V21.txt";
+
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileexists = fileInfo.Exists;
+            bool expect = true;
+            Assert.AreEqual(expect, fileexists);
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint5.Task6.V21/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GairabekovMR.Sprint5.Task6.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
