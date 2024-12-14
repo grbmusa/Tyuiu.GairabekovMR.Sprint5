da11511627083e88dd7b202a94b68bab0f141754
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 08:51:44 +0500

    Task7

---
+++ Tyuiu.GairabekovMR.Sprint5.Task7.V23.Lib/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Tyuiu.GairabekovMR.Sprint5.Task7.V23.Lib
+{
+    public class DataService : ISprint5Task7V23
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V23.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        strLine = Regex.Replace(line, @"\b\p{IsCyrillic}+\b", "");
+                    }
+
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint5.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GairabekovMR.Sprint5.Task7.V23.Lib;
+using System.IO;
+namespace Tyuiu.GairabekovMR.Sprint5.Task7.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\deti2\source\repos\Tyuiu.Gairab.Sprint5\Tyuiu.Gairab.Sprint5.Task7.V23\bin\Debug\OutPutDataFileTask7V23.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
