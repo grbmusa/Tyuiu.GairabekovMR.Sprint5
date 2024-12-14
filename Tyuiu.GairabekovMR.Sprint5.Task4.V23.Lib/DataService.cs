00370b815c15d264bf67ae30ee34210e9905f816
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 08:00:40 +0500

    Task4

---
+++ Tyuiu.GairabekovMR.Sprint5.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.GairabekovMR.Sprint5.Task4.V23.Lib
+{
+    public class DataService : ISprint5Task4V23
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string n = File.ReadAllText(path);
+            double x = double.Parse(n.Replace('.', ','));
+            double result = Math.Pow(x, -3) + 2 + Math.Cos(x);
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint5.Task4.V23/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.GairabekovMR.Sprint5.Task4.V23.Lib;
+namespace Tyuiu.GairabekovMR.Sprint5.Task4.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Гайрабеков | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5");
+            Console.WriteLine("* Тема: Чтение данных");
+            Console.WriteLine("* Задание #4");
+            Console.WriteLine("* Вариант #23");
+            Console.WriteLine("* Выполнил: Гайрабеков | ИИПБ-24-2");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:Вычислить значение по формуле (Полученное значение округлить до трёх знаков после запятой) " +
+                "и вернуть полученный результат на консоль.");
+            Console.WriteLine("");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("***************************************************************************");
+            string path = Path.Combine(Path.GetTempPath(), "DataSprint5", "InPutDataFileTask4V23.txt");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Данные находятся в: {path}");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Ответ:" + res);
+            Console.ReadKey();
+        }
+    }
+}
