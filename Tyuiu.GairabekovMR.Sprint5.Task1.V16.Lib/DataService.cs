b822687f56937ed164a2713e2d2618eabbf7a3d2
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 07:15:28 +0500

    Task1

--- Tyuiu.GairabekovMR.Sprint5.Task1.V16.Lib/DataService.cs
+++ Tyuiu.GairabekovMR.Sprint5.Task1.V16.Lib/DataService.cs
@@ -1,49 +1,23 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
-namespace Tyuiu.GairabekovMR.Sprint5.Task1.V16.Lib
+namespace Tyuiu.GairabekovMR.Sprint5.Task1.V16.Lib;
+public class DataService : ISprint5Task1V16
 {
-    public class DataService : ISprint5Task1V16
+    public string SaveToFileTextData(int startValue, int stopValue)
     {
-        public string SaveToFileTextData(int startValue, int stopValue)
+        string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+
+        while (startValue <= stopValue)
         {
-            string path = Path.Combine(Convert.ToString(Path.GetTempPath()), "OutPutFileTask1.txt");
+            double result = Math.Sin(startValue) + (2 * startValue) / 3 - Math.Cos(startValue) * 4 * startValue;
+            if (double.IsInfinity(result) || double.IsNaN(result))
+                result = 0;
+            result = Math.Round(result, 2);
 
-            FileInfo fileInfo = new FileInfo(path);
-            bool fileExists = fileInfo.Exists;
-
-            if (fileExists)
-            {
-                File.Delete(path);
-
-            }
-
-            double y;
-            string stry;
-
-            for (int x = startValue; x <= stopValue; x++)
-            {
-                y = Math.Sin(x) + (2 * x / 3) - Math.Cos(x) * 4 * x;
-                y = Math.Round(y, 2);
-                if (double.IsNaN(y))
-                {
-                    y = 0;
-                }
-                stry = Convert.ToString(y);
-
-
-                if (x < stopValue)
-                {
-                    File.AppendAllText(path, $"{stry}" + Environment.NewLine);
-
-                }
-                else
-                {
-                    File.AppendAllText(path, $"{stry}");
-                }
-
-            }
-
-            return path;
+            File.AppendAllText(path, $"{result.ToString()}\n");
+            startValue++;
         }
+
+        return path;
     }
 }
